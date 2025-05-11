425f4b1495959fa4d14331ab014215f79a7a9756
Marcos Barbosa <marmbmti@gmail.com>
2025-05-11 08:50:42 -0300

    refactor: removing functions that are no longer used

--- src/controllers/helpers/response.ts
+++ src/controllers/helpers/response.ts
@@ -1,35 +1,5 @@
 import { badRequestResponse } from "./http";
 
-export function invalidPasswordResponse() {
-  return badRequestResponse("Password must be at least 6 characters");
-}
-
-export function invalidEmailResponse() {
-  return badRequestResponse("Email invalid. Please, provider a valid one");
-}
-
-export function invalidUuidResponse() {
-  return badRequestResponse("Uuid invalid. Please, provider a valid one");
-}
-
 export function invalidParamsResponse() {
   return badRequestResponse("Params invalid");
-}
-
-export function invalidRequiredFieldsResponse(field: string) {
-  return badRequestResponse(`Missing param: ${field}`);
-}
-
-export function invalidAmountResponse() {
-  return badRequestResponse("The amount must be a valid currency");
-}
-
-export function invalidTransactionTypeResponse() {
-  return badRequestResponse("Transaction type must be 'EARNING' or 'EXPENSE'");
-}
-
-export function invalidDateResponse() {
-  return badRequestResponse(
-    "Date invalid. Please, provider a valid date with the format (YYY-MM-DD).",
-  );
 }
--- src/controllers/helpers/validators.ts
+++ src/controllers/helpers/validators.ts
@@ -1,9 +1,5 @@
 import validator from "validator";
 
-export function checkIfUuidIsValid(id: string) {
-  return validator.isUUID(id);
-}
-
 export function checkIfPasswordIsValid(password: string) {
   return validator.isStrongPassword(password, {
     minLength: 6,
@@ -12,37 +8,6 @@ export function checkIfPasswordIsValid(password: string) {
     minLowercase: 0,
     minUppercase: 0,
   });
-}
-
-export function checkIfEmailIsValid(email: string) {
-  return validator.isEmail(email);
-}
-
-export function checkIfRequiredFieldsIsInvalid(data: any, fields: string[]) {
-  for (const field of fields) {
-    if (data[field] === undefined) {
-      return field;
-    }
-
-    if (String(data[field]).trim() === "") {
-      return field;
-    }
-  }
-
-  return null;
-}
-
-export function checkIfValueIsEmpty(value: string) {
-  return validator.isEmpty(value);
-}
-
-export function checkIfHasDisallowedFields(data: any, fields: string[]) {
-  const allowedField = fields;
-  const someFieldsNotAllowed = Object.keys(data).some(
-    (field) => !allowedField.includes(field),
-  );
-
-  return someFieldsNotAllowed;
 }
 
 export function checkIfAmountIsCurrency(amount: string) {
@@ -55,12 +20,6 @@ export function checkIfAmountIsCurrency(amount: string) {
     allow_decimal: true,
     decimal_separator: ".",
   });
-}
-
-export function checkIfTransactionTypeIsValid(type: string) {
-  const types = ["EARNING", "EXPENSE"];
-
-  return types.includes(type);
 }
 
 export function checkIfDateIsValid(date: string) {
