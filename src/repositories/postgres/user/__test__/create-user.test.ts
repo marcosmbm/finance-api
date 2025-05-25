d552693fb42a2737c7f81865acbd0a395e0e0db4
Marcos Barbosa <marmbmti@gmail.com>
2025-05-25 08:13:19 -0300

    test: ensure create user repository calls prisma with correct params

--- src/repositories/postgres/user/__test__/create-user.test.ts
+++ src/repositories/postgres/user/__test__/create-user.test.ts
@@ -1,6 +1,7 @@
-import { describe, expect, it } from "@jest/globals";
+import { describe, expect, it, jest } from "@jest/globals";
 import { CreateUserRepository } from "../create-user";
 import { fixtureUser } from "@/tests";
+import { prisma } from "@/db/prisma";
 
 describe("Create user repository test", () => {
   it("should create a user on db", async () => {
@@ -12,5 +13,28 @@ describe("Create user repository test", () => {
     expect(result.id).toBe(fixtureUser.id);
     expect(result.first_name).toBe(fixtureUser.first_name);
     expect(result.last_name).toBe(fixtureUser.last_name);
+  });
+
+  it("should call prisma with correct params", async () => {
+    const sut = new CreateUserRepository();
+    const prismaSpy = jest.spyOn(prisma.user, "create");
+
+    await sut.execute(fixtureUser);
+
+    expect(prismaSpy).toHaveBeenCalledWith({
+      data: {
+        id: fixtureUser.id,
+        first_name: fixtureUser.first_name,
+        last_name: fixtureUser.last_name,
+        email: fixtureUser.email,
+        password: fixtureUser.password,
+      },
+      select: {
+        id: true,
+        first_name: true,
+        last_name: true,
+        email: true,
+      },
+    });
   });
 });
