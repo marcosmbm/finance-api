31348000501995a083e7d52d6d0bbfb203ea6a5a
Marcos Barbosa <marmbmti@gmail.com>
2025-05-30 11:43:42 -0300

    test: ensure makeUpdateUserController returns valid

--- src/factories/controllers/__test__/user.test.ts
+++ src/factories/controllers/__test__/user.test.ts
@@ -1,9 +1,13 @@
 import { describe, expect, it } from "@jest/globals";
-import { makeCreateUserController } from "../user";
-import { CreateUserController } from "@/controllers";
+import { makeCreateUserController, makeUpdateUserController } from "../user";
+import { CreateUserController, UpdateUserController } from "@/controllers";
 
 describe("User controller factories test", () => {
   it("Should return a valid CreateUserController instance", () => {
     expect(makeCreateUserController()).toBeInstanceOf(CreateUserController);
+  });
+
+  it("Should return a valid UpdateUserController instance", () => {
+    expect(makeUpdateUserController()).toBeInstanceOf(UpdateUserController);
   });
 });
