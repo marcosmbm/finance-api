d998f6d66c90ceb9b749f431f2bf9c4b4b889200
Marcos Barbosa <marmbmti@gmail.com>
2025-05-30 08:41:21 -0300

    test: ensure makeUpdateTransactionController returns valid

--- src/factories/controllers/__test__/transaction.test.ts
+++ src/factories/controllers/__test__/transaction.test.ts
@@ -1,11 +1,23 @@
 import { describe, expect, it } from "@jest/globals";
-import { makeCreateTransactionController } from "../transaction";
-import { CreateTransactionController } from "@/controllers";
+import {
+  makeCreateTransactionController,
+  makeUpdateTransactionController,
+} from "../transaction";
+import {
+  CreateTransactionController,
+  UpdateTransactionController,
+} from "@/controllers";
 
 describe("Transaction controller factories test", () => {
   it("Should return a valid CreateTransactionController instance", () => {
     expect(makeCreateTransactionController()).toBeInstanceOf(
       CreateTransactionController,
+    );
+  });
+
+  it("Should return a valid UpdateTransactionController instance", () => {
+    expect(makeUpdateTransactionController()).toBeInstanceOf(
+      UpdateTransactionController,
     );
   });
 });
