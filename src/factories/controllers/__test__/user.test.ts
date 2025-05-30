2e54f6462be04b55a58c673fb007cc234b93bd5b
Marcos Barbosa <marmbmti@gmail.com>
2025-05-30 16:06:18 -0300

    test: ensure makeGetUserByIdController returns valid

--- src/factories/controllers/__test__/user.test.ts
+++ src/factories/controllers/__test__/user.test.ts
@@ -1,6 +1,14 @@
 import { describe, expect, it } from "@jest/globals";
-import { makeCreateUserController, makeUpdateUserController } from "../user";
-import { CreateUserController, UpdateUserController } from "@/controllers";
+import {
+  makeCreateUserController,
+  makeGetUserByIdController,
+  makeUpdateUserController,
+} from "../user";
+import {
+  CreateUserController,
+  GetUserByIdController,
+  UpdateUserController,
+} from "@/controllers";
 
 describe("User controller factories test", () => {
   it("Should return a valid CreateUserController instance", () => {
@@ -9,5 +17,9 @@ describe("User controller factories test", () => {
 
   it("Should return a valid UpdateUserController instance", () => {
     expect(makeUpdateUserController()).toBeInstanceOf(UpdateUserController);
+  });
+
+  it("Should return a valid GetUserByIdController instance", () => {
+    expect(makeGetUserByIdController()).toBeInstanceOf(GetUserByIdController);
   });
 });
