818349ea85cc83765f01ab23078357b2f5f1203f
Marcos Barbosa <marmbmti@gmail.com>
2025-06-12 08:29:57 -0300

    fix: changing the name of the server port variable to port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "module-alias/register";
 
 import { app } from "./app";
 
-const port = process.env.SERVER_PORT;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
